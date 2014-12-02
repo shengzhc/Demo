6f4bb16cded9a52ee0292516088152554dde3ffe
Shengzhe Chen <shengzhe.chen@gmail.com>
2014-12-01 16:23:00 -0800

    adding framework supports

--- DemoApp/DemoApp/Demo_SCModalDialog.swift
+++ DemoApp/DemoApp/Demo_SCModalDialog.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import SCModalDialog
 
 class Demo_ModalDialogViewController: UIViewController, SCModalDialogPresentationControllerDelegate
 {
---
+++ SCModalDialog/SCModalDialog/SCModalDialog.h
@@ -0,0 +1,19 @@
+//
+//  SCModalDialog.h
+//  SCModalDialog
+//
+//  Created by Shengzhe Chen on 12/1/14.
+//  Copyright (c) 2014 Shengzhe Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SCModalDialog.
+FOUNDATION_EXPORT double SCModalDialogVersionNumber;
+
+//! Project version string for SCModalDialog.
+FOUNDATION_EXPORT const unsigned char SCModalDialogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SCModalDialog/PublicHeader.h>
+
+
