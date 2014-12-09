86bb5af753291eda0b3ab6c39688d543a05096bf
Shengzhe Chen <shengzhe.chen@gmail.com>
2014-12-08 17:24:31 -0800

    update

--- DemoApp/DemoApp/SCPhotoViewer.swift
+++ DemoApp/DemoApp/SCPhotoViewer.swift
@@ -12,19 +12,80 @@ import UIKit
 public class SCZoomingImageView: UIScrollView, UIScrollViewDelegate
 {
     private var _imageView = UIImageView()
+    private var doubleTapGestureRecognizer: UITapGestureRecognizer?
+    private var singleTapGestureRecognizer: UITapGestureRecognizer?
     
     public var imageView: UIImageView {
         get {
-            if _imageView.superview == nil {
-                _imageView.contentMode = .ScaleToFill
-                self.delegate = self
-                self.addSubview(_imageView)
-            }
             return _imageView
         }
         set {
             _imageView = newValue
         }
+    }
+    
+    public override init(frame: CGRect)
+    {
+        super.init(frame: frame)
+        self.setup()
+    }
+    
+    public override init()
+    {
+        super.init()
+        self.setup()
+    }
+
+    required public init(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup()
+    {
+        self.setupContent()
+        self.setupGestureRecognizer()
+        self.reset()
+    }
+    
+    public func setupContent()
+    {
+        if self.imageView.superview == nil {
+            self.imageView.contentMode = .ScaleToFill
+            self.delegate = self
+            self.addSubview(self.imageView)
+        }
+    }
+    
+    public func setupGestureRecognizer()
+    {
+        self.singleTapGestureRecognizer = UITapGestureRecognizer(target: self, action: "handleSingleTap:")
+        self.doubleTapGestureRecognizer = UITapGestureRecognizer(target: self, action: "handleDoubleTap:")
+        
+        if let singleTapGestureRecognizer = self.singleTapGestureRecognizer {
+            if let doubleTapGestureRecognizer = self.doubleTapGestureRecognizer {
+                singleTapGestureRecognizer.numberOfTapsRequired = 1
+                doubleTapGestureRecognizer.numberOfTapsRequired = 2
+                singleTapGestureRecognizer.requireGestureRecognizerToFail(doubleTapGestureRecognizer)
+                self.imageView.userInteractionEnabled = true
+                self.imageView.addGestureRecognizer(singleTapGestureRecognizer)
+                self.imageView.addGestureRecognizer(doubleTapGestureRecognizer)
+            }
+        }
+    }
+    
+    public func handleSingleTap(gestureRecognizer: UIGestureRecognizer)
+    {
+    }
+    
+    public func handleDoubleTap(gestureRecognizer: UIGestureRecognizer)
+    {
+        var scalor = self.zoomScale == self.maximumZoomScale ? self.minimumZoomScale : self.maximumZoomScale
+        var location = gestureRecognizer.locationInView(self.imageView)
+        var zoom_rect = CGRectMake(0, 0, self.bounds.size.width/scalor, self.bounds.size.height/scalor)
+        zoom_rect.origin.x = location.x - zoom_rect.size.width/2.0
+        zoom_rect.origin.y = location.y - zoom_rect.size.height/2.0
+        self.zoomToRect(zoom_rect, animated: true)
     }
     
     public override func layoutSubviews()
@@ -40,7 +101,6 @@ public class SCZoomingImageView: UIScrollView, UIScrollViewDelegate
     public func scrollViewDidZoom(scrollView: UIScrollView)
     {
         self.alignContent()
-        println("\(scrollView.contentOffset), \(scrollView.contentSize)")
     }
     
     public func alignContent()
